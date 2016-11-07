2c7e0eca71b5e85cd3d39a0894c4bb216e41426a
Matt Ferguson <mattferguson@Matts-Mac.local>
2016-11-07 13:57:36 -0500

    Small changes to the task view and updated the readme

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -23,7 +23,7 @@
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Add Task
+                        Add Task
                     </button>
                 </div>
             </div>
